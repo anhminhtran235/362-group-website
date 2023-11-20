34c360feb4ed2041994151280b4a2a2fbd45b2d0
Anh Minh Tran <anhminhtran235@gmail.com>
2023-11-20 15:38:36 -0800

    Add video for show and tell 1

--- src/pages/ShowAndTell1.js
+++ src/pages/ShowAndTell1.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const SHOW_AND_TELL_1_SLIDES_EMBED_CODE = `<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRkThVHBcmeEoDhw4Ts71u9xfiWUj4GVVUafGnpaUYYSJ9UY66WUcEMIN-watM8aWfQ1qpnuTvNlvf3/embed?start=false&loop=false&delayms=3000" frameborder="0" width="720" height="440" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>`;
 
-const SHOW_AND_TELL_1_VIDEO_EMBED_CODE = `<iframe width="720" height="400" src="https://www.youtube.com/embed/AVObd9AqXco" title="LocaDine - CMPT 362 Group 13" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+const SHOW_AND_TELL_1_VIDEO_EMBED_CODE = `<iframe width="720" height="400" src="https://www.youtube.com/embed/2-KE6hEcI4k" title="Show and tell 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
 
 const ShowAndTell1Style = styled.div`
   display: flex;
